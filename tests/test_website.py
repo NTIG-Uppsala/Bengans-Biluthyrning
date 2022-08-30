c87b3fd473e0d8e08227fcc73cfd17eb91a163fe
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-08-30 13:39:22 +0200

    changed comments to english

--- tests/test_website.py
+++ tests/test_website.py
@@ -1,4 +1,4 @@
-# Utgår ifrån https://github.com/jsoma/selenium-github-actions
+# based on https://github.com/jsoma/selenium-github-actions
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,16 +12,16 @@ class CheckSiteAvailability(unittest.TestCase):
         it means that the website isn't online and running
     """
     def setup(self):
-        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # installerar drivern i minnet
+        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # installing the driver to memory
 
         chrome_options = Options() # 
         chrome_options.add_argument("--headless") # 
 
-        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initiliserar chrome drivern från den nerladdade
-        self.addCleanup(self.browser.quit) # stäng webläsaren när testen är klar
+        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initializes chrome driver from the local instance of chrome
+        self.addCleanup(self.browser.quit) # close the browser when the tests are done
 
-        self.website_url = "https://ntig-uppsala.github.io/bengans-biluthyrning/" # Urln som kommer användas
-        # self.website_url = "http://127.0.0.1:5500/src/" # Urln som kommer användas
+        self.website_url = "https://ntig-uppsala.github.io/bengans-biluthyrning/" # url to be used
+        # self.website_url = "http://127.0.0.1:5500/src/" # url that will be used
 
     def test_page_title(self):
         self.browser.get(self.website_url)
@@ -29,10 +29,7 @@ class CheckSiteAvailability(unittest.TestCase):
 
     def test_page_text(self):
         self.browser.get(self.website_url)
-        self.assertIn("Bengans Biluthyrning", self.browser.find_element(By.TAG_NAME, "body"))
-    
-        #self.assertIn(info_value, self.browser.find_element(By.TAG_NAME, "body").text)
+        self.assertIn("Bengans Biluthyrning", self.browser.find_element(By.TAG_NAME, "body").text)
 
-
-if __name__ == '__main__':      #Kör igenom all kod i testet
-    unittest.main(verbosity=69)  #Hur mycket information som ges av testerna
+if __name__ == '__main__':      #runs all code in the test
+    unittest.main(verbosity=69)  #how much info to be output to terminal
