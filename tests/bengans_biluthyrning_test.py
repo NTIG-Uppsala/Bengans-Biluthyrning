9d08cdef56f315220d2355f072ec085a5591f92d
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-09-01 15:19:40 +0200

    changed variable name to comply with naming standard
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>

--- tests/bengans_biluthyrning_test.py
+++ tests/bengans_biluthyrning_test.py
@@ -1,12 +1,12 @@
 from seleniumbase import BaseCase  #importing testing framework
 import os
 
-startPage = "https://ntig-uppsala.github.io/Bengans-Biluthyrning/"  #website url
+start_page = "https://ntig-uppsala.github.io/Bengans-Biluthyrning/"  #website url
 
 class company(BaseCase):
     def test_text(self):   #defining first test
-        self.open(startPage) #opens the page in browser
+        self.open(start_page) #opens the page in browser
         self.assert_text("Bengans Biluthyrning", "body")    #checks for "Bengans Biluthyrning" in body
     def test_title(self):
-        self.open(startPage)
+        self.open(start_page)
         self.assert_title("Bengans Biluthyrning")   #checks for "Bengans Biluthyrning" in title
