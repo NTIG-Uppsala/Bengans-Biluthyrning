2f263a20a657e705421600416edd5fdb64685230
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-09-01 09:50:07 +0200

    Added test for title
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>

--- tests/bengans_biluthyrning_test.py
+++ tests/bengans_biluthyrning_test.py
@@ -7,3 +7,8 @@ class company(BaseCase):
     def test_start(self):
         self.open(startPage)
         self.assert_text("Bengans Biluthyrning", "body")
+    def test_title(self):
+        self.open(startPage)
+        self.assert_title("Bengans Biluthyrning")
+
+#Loop function instead of creating multiple
