de7fb50b6d90f80927cf8dae537acbccf04693c0
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-09-06 11:57:45 +0200

    Updated test file

--- tests/bengans_biluthyrning_test.py
+++ tests/bengans_biluthyrning_test.py
@@ -1,13 +1,46 @@
 from seleniumbase import BaseCase  #importing testing framework
-import os
 import pathlib
 
-start_page = "file:///" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/") + "index.html" #website url
+startPage = "file:///" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/") + "index.html" #website url
 
-class company(BaseCase):
-    def test_text(self):   #defining first test
-        self.open(start_page) #opens the page in browser
+# listList = ["Random bullshit", "random stuff"]
+
+class workingWebsite(BaseCase):
+    def testText(self):   #defining first test
+        self.open(startPage) #opens the page in browser
         self.assert_text("Bengans Biluthyrning", "body")    #checks for "Bengans Biluthyrning" in body
-    def test_title(self):
-        self.open(start_page)
+    def testTitle(self):
+        self.open(startPage)
         self.assert_title("Bengans Biluthyrning")   #checks for "Bengans Biluthyrning" in title
+
+class basicInformation(BaseCase):
+    def testContactInfo(self):   
+        self.open(startPage)
+        self.assert_text("Adress", "body")
+        self.assert_text("Öpettider", "body")
+        self.assert_text("Telefonnummer", "body")
+        self.assert_text("Mail", "body")
+    def testIcons(self):
+        self.open(startPage)
+        self.is_element_present("#twitter", by="css selector")
+        self.click("#twitter", by="css selector", timeout=None, delay=0, scroll=True)
+        self.get_current_url()
+        self.open(startPage)
+        self.is_element_present("#facebook", by="css selector")
+        self.click("#facebook", by="css selector", timeout=None, delay=0, scroll=True)
+        self.get_current_url()
+        self.open(startPage)
+        self.is_element_present("#instagram", by="css selector")
+        self.click("#instagram", by="css selector", timeout=None, delay=0, scroll=True)
+        self.get_current_url()
+
+class imagesProducts(BaseCase):
+    def testText(self):  
+        pass
+        #self.open another page
+        #check for images
+        #check for products
+        #check for logo
+
+
+# Överrubriker i backlogg är klasser, underrubrik är funktioner
