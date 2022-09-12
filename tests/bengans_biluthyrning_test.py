6ba64ca51aad2c59d7c2477a7d2de790c7408a66
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-09-12 11:36:29 +0200

    Formatted test file to follow PEP-8
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>

--- tests/bengans_biluthyrning_test.py
+++ tests/bengans_biluthyrning_test.py
@@ -1,54 +1,74 @@
-from seleniumbase import BaseCase  #importing testing framework
+from seleniumbase import BaseCase  # importing testing framework
 import pathlib
 
 # To start the test, run "python -m pytest .\bengans_biluthyrning_test.py" in "\Bengans-Biluthyrning\tests"
 # or "python -m pytest .\tests\bengans_biluthyrning_test.py" in "\Bengans-Biluthyrning"
 
-startPage = "file://" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/") + "index.html" # Find location of index.html and prepare formatting
+startPage = "file://" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace(
+    "\\", "/") + "index.html"  # Find location of index.html and prepare formatting
 
-basicInfoTexts = ["Fjällgatan 32H, 981 39 Jönköping", "Vardagar: 10-16", "Lördagar: 12-15", "Söndagar: Stängt", "0630-555-555", "info@<DOMÄN>"] # Basic info to test for
+basicInfoTexts = ["Fjällgatan 32H, 981 39 Jönköping", "Vardagar: 10-16", "Lördagar: 12-15",
+                  "Söndagar: Stängt", "0630-555-555", "info@<DOMÄN>"]  # Basic info to test for
 
-socialMediaPaths = ["src/images/facebook.svg", "src/images/twitter.svg", "src/images/instagram.svg"] # Paths for different social media .svg imgs
-socialLinks = ["https://sv-se.facebook.com/ntiuppsala/", "https://twitter.com/ntiuppsala", "https://www.instagram.com/ntiuppsala/"] # Relevant social media links
+socialMediaPaths = ["src/images/facebook.svg", "src/images/twitter.svg",
+                    "src/images/instagram.svg"]  # Paths for different social media .svg imgs
+socialLinks = ["https://sv-se.facebook.com/ntiuppsala/", "https://twitter.com/ntiuppsala",
+               "https://www.instagram.com/ntiuppsala/"]  # Relevant social media links
 
-picturePaths = ["src/images/bild1.jpg", "src/images/bild2.jpg", "src/images/bild3.jpg"] # Paths for the images
+picturePaths = ["src/images/bild1.jpg", "src/images/bild2.jpg",
+                "src/images/bild3.jpg"]  # Paths for the images
 
-productList = ["Audi A6", "Renault Kadjar", "Kia Soul", "Subaru Outback", "Caddilac Escalade", "Mitshubichi Outlander", "Volvo XC40", "VW Polo", "Kia Carens"] # List of products
+productList = ["Audi A6", "Renault Kadjar", "Kia Soul", "Subaru Outback", "Caddilac Escalade",
+               "Mitshubichi Outlander", "Volvo XC40", "VW Polo", "Kia Carens"]  # List of products
+
 
 class workingWebsite(BaseCase):
     def testText(self):
-        self.open(startPage) # Open the page in browser
-        self.assert_text("Bengans Biluthyrning", "body") # Check for "Bengans Biluthyrning" in body
+        self.open(startPage)  # Open the page in browser
+        # Check for "Bengans Biluthyrning" in body
+        self.assert_text("Bengans Biluthyrning", "body")
+
     def testTitle(self):
         self.open(startPage)
-        self.assert_title("Bengans Biluthyrning") # Check for "Bengans Biluthyrning" in title
+        # Check for "Bengans Biluthyrning" in title
+        self.assert_title("Bengans Biluthyrning")
+
 
 class basicInformation(BaseCase):
-    def testContactInfo(self):   
+    def testContactInfo(self):
         self.open(startPage)
         for i in basicInfoTexts:
-            self.assert_text(i, "body") # Check for all info in basicInfoTexts[]
+            # Check for all info in basicInfoTexts[]
+            self.assert_text(i, "body")
+
     def testIcons(self):
         for i in range(len(socialMediaPaths)):
             self.open(startPage)
-            self.click(f"[src=\"{socialMediaPaths[i]}\"]") # Check that the icon link exists, and clicks if it does
-            if(self.get_current_url() != socialLinks[i]): # Checks that links lead to the right place
+            # Check that the icon link exists, and clicks if it does
+            self.click(f"[src=\"{socialMediaPaths[i]}\"]")
+            # Checks that links lead to the right place
+            if (self.get_current_url() != socialLinks[i]):
                 raise NameError(f"Failed at {socialMediaPaths[i]}")
 
+
 class imagesAndProducts(BaseCase):
-    def testImages(self):  
+    def testImages(self):
         self.open(startPage)        # self.open another page later
         for i in picturePaths:
-            self.assert_element(f"img[src=\"{i}\"]") # Check for the 3 images by path
+            # Check for the 3 images by path
+            self.assert_element(f"img[src=\"{i}\"]")
+
     def testProducts(self):
         self.open(startPage)        # self.open another page later
         for i in productList:
-            self.assert_text(i, "body") # Check for products
+            self.assert_text(i, "body")  # Check for products
+
     def testLogo(self):
         self.open(startPage)
         # It is not possible to test for the content of <link>s, therefore we have decided to treat the favicon like a page design, untestable and therefore passed by default.
         # self.assert_element("head > link[sizes=\"32x32\"][href=\"src/images/icons/favicon-32x32.png\"]") #Checks for a link to 32x32 favicon in head
-        self.assert_element("img[src=\"src/images/logo.svg\"]") # Check for an image with the ID #logo
+        # Check for an image with the ID #logo
+        self.assert_element("img[src=\"src/images/logo.svg\"]")
         # Repeat for all pages once we add more
 
 
