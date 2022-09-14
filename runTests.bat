e2a5419ce0d64504b44282ca49486eaa94f508ad
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-09-14 15:14:26 +0200

    Update runTests.bat
    Co-Authored-By: Leon Lööv <111981993+Leon-Loov@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>

--- runTests.bat
+++ runTests.bat
@@ -1,6 +1,6 @@
 :: start "" creates a new cmd window
-start "" python -m pytest .\tests\indexTest.py
-start "" python -m pytest .\tests\productTest.pytest
-
+start /b python -m pytest .\tests\indexTest.py
+start /b python -m pytest .\tests\productTest.py
+pause
 :: python -m pytest .\tests\mainTest.py
 :: python -m pytest .\tests\mainTestCopy.py
