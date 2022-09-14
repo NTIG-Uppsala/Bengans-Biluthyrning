15197c60ca63d9342b2a9c1915bb6720aded7e3e
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-09-14 09:34:27 +0200

    Created a bet file to run the tests
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>

---
+++ runTests.bat
@@ -0,0 +1,5 @@
+:: start "" creates a new cmd window
+:: start "" python -m pytest .\tests\mainTest.py
+:: start "" python -m pytest .\tests\mainTestCopy.py
+python -m pytest .\tests\mainTest.py
+python -m pytest .\tests\mainTestCopy.py
