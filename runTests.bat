5204ecbe38017c9a06a06cc26f2a1c932a1cf551
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-09-20 12:50:55 +0200

    Made the tests run headless

--- runTests.bat
+++ runTests.bat
@@ -1,5 +1,5 @@
 :: start "" creates a new cmd window
-start /b python -m pytest .\tests\indexTest.py
-start /b python -m pytest .\tests\productTest.py & pause
+start /b python -m pytest .\tests\indexTest.py --headless
+start /b python -m pytest .\tests\productTest.py --headless & pause
 :: python -m pytest .\tests\mainTest.py
 :: python -m pytest .\tests\mainTestCopy.py
