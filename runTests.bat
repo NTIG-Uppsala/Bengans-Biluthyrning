13ba26d1ab3267fc0df8a2cd62c26d32f016ddcd
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-09-22 14:40:24 +0200

    Updated footer formatting

--- runTests.bat
+++ runTests.bat
@@ -1,6 +1,7 @@
 :: start "" creates a new cmd window
 start /b python -m pytest .\tests\indexTest.py --headless --settings-file=.\tests\customSettings.py
 start /b python -m pytest .\tests\productTest.py --headless --settings-file=.\tests\customSettings.py
+start /b python -m pytest .\tests\employeeTest.py --headless --settings-file=.\tests\customSettings.py
 start /b python -m pytest .\tests\indexPhoneTest.py --headless --settings-file=.\tests\phoneSettings.py
 start /b python -m pytest .\tests\productPhoneTest.py --headless --settings-file=.\tests\phoneSettings.py & pause
 :: python -m pytest .\tests\mainTest.py
