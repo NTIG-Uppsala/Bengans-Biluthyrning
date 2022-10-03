5c3a5eebef89de4dd4381ef85ae56c42297209e7
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2022-10-03 14:42:21 +0200

    Updated website generator (WIP)
    Co-Authored-By: Leo Kempe <leo.max.kempe@gmail.com>
    Co-Authored-By: lukas-laas <111971517+lukas-laas@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>

--- dev/klonahtml.py
+++ dev/klonahtml.py
@@ -4,6 +4,18 @@ versions = {
     "dev/staffDEV.html": ["personal.html", "EN/staff.html"]
 }
 
+cards = {
+    "audiA6": ["audiA6.jpg", "Audi A6", "800"], 
+    "renaultKadjar": [None, "Renault Kadjar", "450"],
+    "kiaSoul": [None, "Kia Soul", "400"],
+    "subaruOutback": ["subaruOutback.jpg", "Subaru Outback", "300"],
+    "cadillacEscalade": ["cadillacEscalade.jpg", "Cadillac Escalade", "500"],
+    "mitsubishiOutlander": ["mitsubishiOutlander.jpg", "Mitsubishi Outlander", "450"],
+    "volvoXC40": ["volvoXC40.jpg", "Volvo XC40", "800"],
+    "vwPolo": ["vwPolo.jpg", "VW Polo", "300"],
+    "kiaCarens": [None, "Kia Carens", "500"]
+}
+
 def makeWebsite():
     for i in versions:
         data = open(i, 'r', encoding='utf-8')
@@ -18,5 +30,13 @@ def makeWebsite():
             else:
                 f_sv.write(str(line))
                 f_en.write(str(line))
+            if '<!--Cards go here-->' in str(line):
+                for j in cards:
+                    card = str(line).split("<!--Cards go here-->")[0]+'<div class="productsMainFlexCard" id="{}">\n'.format(j)
+                    card += str(line).split("<!--Cards go here-->")[0]+'<div class="cardImage">\n'
+                    if cards [j][0] != None:
+                        card += str(line).split("<!--Cards go here-->")[0]+'\t<img src="src/images/products/{}">\n'.format(cards[j][0])
+                    card += str(line).split("<!--Cards go here-->")[0]+'</div>\n'
+                    f_sv.write(card+str(line).split("<!--Cards go here-->")[0]+"</div>\n")
 
 makeWebsite()
