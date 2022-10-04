6d50be75df549621aed5911615642e2382acea67
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2022-10-04 10:31:51 +0200

    Commented out unfinished code (WIP)
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Leo Kempe <leo.max.kempe@gmail.com>
    Co-Authored-By: lukas-laas <111971517+lukas-laas@users.noreply.github.com>

--- dev/klonahtml.py
+++ dev/klonahtml.py
@@ -4,17 +4,17 @@ versions = {
     "dev/staffDEV.html": ["personal.html", "EN/staff.html"]
 }
 
-cards = {
-    "audiA6": ["audiA6.jpg", "Audi A6", "800"], 
-    "renaultKadjar": [None, "Renault Kadjar", "450"],
-    "kiaSoul": [None, "Kia Soul", "400"],
-    "subaruOutback": ["subaruOutback.jpg", "Subaru Outback", "300"],
-    "cadillacEscalade": ["cadillacEscalade.jpg", "Cadillac Escalade", "500"],
-    "mitsubishiOutlander": ["mitsubishiOutlander.jpg", "Mitsubishi Outlander", "450"],
-    "volvoXC40": ["volvoXC40.jpg", "Volvo XC40", "800"],
-    "vwPolo": ["vwPolo.jpg", "VW Polo", "300"],
-    "kiaCarens": [None, "Kia Carens", "500"]
-}
+# cards = {
+#     "audiA6": ["audiA6.jpg", "Audi A6", "Automat", "Automatic", "800", "PLACEHOLDER"], 
+#     "renaultKadjar": [None, "Renault Kadjar", "Automat", "Automatic", "450", "PLACEHOLDER"],
+#     "kiaSoul": [None, "Kia Soul", "Manuell", "Manual", "400", "PLACEHOLDER"],
+#     "subaruOutback": ["subaruOutback.jpg", "Subaru Outback", "Manuell", "Manual", "300", "PLACEHOLDER"],
+#     "cadillacEscalade": ["cadillacEscalade.jpg", "Cadillac Escalade", "Manuell", "Manual", "500", "PLACEHOLDER"],
+#     "mitsubishiOutlander": ["mitsubishiOutlander.jpg", "Mitsubishi Outlander", "Manuell", "Manual", "450", "PLACEHOLDER"],
+#     "volvoXC40": ["volvoXC40.jpg", "Volvo XC40", "Automat", "Automatic", "800", "PLACEHOLDER"],
+#     "vwPolo": ["vwPolo.jpg", "VW Polo", "Manuell", "Manual", "300", "PLACEHOLDER"],
+#     "kiaCarens": [None, "Kia Carens", "Manuell", "Manual", "500", "PLACEHOLDER"]
+# }
 
 def makeWebsite():
     for i in versions:
@@ -30,13 +30,22 @@ def makeWebsite():
             else:
                 f_sv.write(str(line))
                 f_en.write(str(line))
-            if '<!--Cards go here-->' in str(line):
-                for j in cards:
-                    card = str(line).split("<!--Cards go here-->")[0]+'<div class="productsMainFlexCard" id="{}">\n'.format(j)
-                    card += str(line).split("<!--Cards go here-->")[0]+'<div class="cardImage">\n'
-                    if cards [j][0] != None:
-                        card += str(line).split("<!--Cards go here-->")[0]+'\t<img src="src/images/products/{}">\n'.format(cards[j][0])
-                    card += str(line).split("<!--Cards go here-->")[0]+'</div>\n'
-                    f_sv.write(card+str(line).split("<!--Cards go here-->")[0]+"</div>\n")
+            # if '<!--Cards go here-->' in str(line):
+            #     for j in cards:
+            #         cardStart = str(line).split("<!--Cards go here-->")[0]+'<div class="productsMainFlexCard" id="{}">\n'.format(j)
+            #         cardStart += str(line).split("<!--Cards go here-->")[0]+'<div class="cardImage">\n'
+            #         cardSV = ""
+            #         cardEN = ""
+            #         if cards [j][0] != None:
+            #             cardSV += str(line).split("<!--Cards go here-->")[0]+'\t<img src="src/images/products/{}">\n'.format(cards[j][0])
+            #             cardEN += str(line).split("<!--Cards go here-->")[0]+'\t<img src="src/images/products/{}">\n'.format(cards[j][0])
+            #         cardStart += str(line).split("<!--Cards go here-->")[0]+'</div>\n'
+            #         cardStart += str(line).split("<!--Cards go here-->")[0]+'<div class="cardText">\n<h1>{}</h1>\n'.format(cards[j][1])
+            #         cardSV += str(line).split("<!--Cards go here-->")[0]+'<p>{}</p>'.format(cards[j][2])
+            #         cardEN += str(line).split("<!--Cards go here-->")[0]+'<p>{}</p>'.format(cards[j][3])
+            #         cardEnd = str(line).split("<!--Cards go here-->")[0]+'</div>\n'                   
+            #         cardEnd += str(line).split("<!--Cards go here-->")[0]+"</div>\n"
+            #         f_sv.write(cardStart + cardSV + cardEnd)
+            #         f_en.write(cardStart + cardEN + cardEnd)
 
 makeWebsite()
